4bea34d9bbd830a5ccd399c8a0802f8effb4e681
Cameron Walworth <cameron.walworth@gmail.com>
2018-08-17 12:32:44 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import BookList from './containers/book_list';
+import BookDetail from './containers/book_detail';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BookList />
+        <BookDetail />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Old Man's War", pages: 380 },
+    { title: "A Connecticut Yankee in King Aurthur's Court", pages: 680 },
+    { title: 'Atlas Shrugged', pages: 1381 },
+    { title: 'Forever War', pages: 455 },
+  ];
+}
